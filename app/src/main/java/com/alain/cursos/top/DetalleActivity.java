74017ecd82b759526dd99dd79faed91cbf561a93
Jonathan Sanchez <jonulisanpar@gmail.com>
2021-06-13 00:03:44 -0500

    Dark theme, NavigationIcon progresivo parte 1: Material Theming

--- app/src/main/java/com/alain/cursos/top/DetalleActivity.java
+++ app/src/main/java/com/alain/cursos/top/DetalleActivity.java
@@ -143,11 +143,20 @@ public class DetalleActivity extends AppCompatActivity {
 
         toolbarLayout.setExpandedTitleColor(Color.WHITE);
         appBar.addOnOffsetChangedListener((appBarLayout, verticalOffset) -> {
-            if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()) {
+            toolbarLayout.setTitle("appbarLayout = " + appBarLayout.getTotalScrollRange());
+            etNombre.setText("verticalOffset = " + verticalOffset);
+            /*if (Math.abs(verticalOffset) == appBarLayout.getTotalScrollRange()) {
                 toolbar.getNavigationIcon().setTint(Color.BLACK);
             } else {
                 toolbar.getNavigationIcon().setTint(Color.WHITE);
-            }
+            }*/
+
+            float percentage = (float)Math.abs(verticalOffset) / appBarLayout.getTotalScrollRange();
+
+            //int colorValue = percentage;
+            toolbar.getNavigationIcon().setTint(Color.rgb(127,127,127));
+
+            etApellidos.setText("porcentaje = " + percentage);
         });
 
         configTitle();
